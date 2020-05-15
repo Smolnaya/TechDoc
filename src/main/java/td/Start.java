55b7c001391843961ec5b6b347174c0accb95e53
Smolnaya <a.smolnaja@ya.ru>
2020-05-16 01:58:02 +0300

    add style

--- src/main/java/td/Start.java
+++ src/main/java/td/Start.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Start extends Application {
@@ -14,10 +15,14 @@ public class Start extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader loader = new FXMLLoader();
-        Parent root = (Parent) loader.load(getClass().getResourceAsStream("/fxml/startFXML.fxml"));
-        stage.setTitle("TechDoc");
+        Parent root = (Parent) loader.load(getClass().getResourceAsStream("/fxml/start.fxml"));
         Scene scene = new Scene(root, 960, 600);
+        String stylesheet = getClass().getResource("/css/start.css").toExternalForm();
+        scene.getStylesheets().add(stylesheet);
+        stage.setTitle("TechDoc");
+        stage.getIcons().add(new Image("/images/clip.png"));
         stage.setScene(scene);
         stage.show();
+
     }
 }
